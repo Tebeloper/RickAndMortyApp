30884b5cf73f01dbe9ca3504dc31bec7d104ad7e
tebeloper <dimitrisgkarlemos@gmail.com>
2023-07-24 18:58:12 +0300

    building spinner if more characters exist

--- RickAndMortyApp/Views/RMCharacterListView.swift
+++ RickAndMortyApp/Views/RMCharacterListView.swift
@@ -44,6 +44,8 @@ final class RMCharacterListView: UIView {
         collectionView.register(RMCharacterCollectionViewCell.self,
                                 forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier)
         
+        collectionView.register(RMFooterLoadingCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier)
+        
         return collectionView
     }()
     
